d9ed058265ddb068d0f2a63cfceafecd59b550be
Demozath <abeljsmz@gmail.com>
2023-04-30 12:46:29 -0400

    Se le agrega estilo y se ordenan etiquetas de HTML habían <TR> que debían ser <TD>, al model se le modifica la C mayuscula a la clase conexion

--- Model/ModeloConsultarProducto.php
+++ Model/ModeloConsultarProducto.php
@@ -5,7 +5,7 @@ class ModeloConsultarProducto{
 
     public function ConsultarProducto($codigoProducto){
         $query = "SELECT * FROM producto WHERE (CODIGO_PRODUCTO) = ($codigoProducto)";
-        $link = conexion::conecta();
+        $link = Conexion::conecta();
         $resultado = mysqli_query($link, $query);
         mysqli_close($link);
         return $resultado;
