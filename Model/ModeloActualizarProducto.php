7c79aa5cf5a9b2a6f658d47a9f05f2fc8c495321
Demozath <abeljsmz@gmail.com>
2023-05-03 12:27:32 -0400

    Se realizan algunas modificaciones y se ordena el código

---
+++ Model/ModeloActualizarProducto.php
@@ -0,0 +1,42 @@
+<?php 
+include('conexion.php');
+class ModeloActualizarProducto{
+
+
+
+}
+
+
+
+
+
+/*    public function ConsultarProducto($codigoProducto, $sucursal = null){
+        $query = "SELECT * FROM producto WHERE (CODIGO_PRODUCTO) = ($codigoProducto)";
+        if($sucursal !==null){
+            $query.= "AND SUCURSAL_PRODUCTO = '$sucursal'";
+        }
+        $link = conexion::conecta();
+        $resultado = mysqli_query($link, $query);
+        mysqli_close($link);
+        return $resultado;
+    }
+        public function ConsultarProductoNombre($nombreProducto, $sucursal =null){
+        $queryNombre = "SELECT * FROM PRODUCTO WHERE (NOMBRE_PRODUCTO) like ('%$nombreProducto%')";
+        if($sucursal !==null){
+            $queryNombre .= "AND SUCURSAL_PRODUCTO = '$sucursal'";
+        }
+        $link = Conexion::conecta();
+        $resultadoNombre = mysqli_query($link, $queryNombre);
+        mysqli_close($link);
+        return $resultadoNombre;
+        }
+        public function ConsultarSucursal($sucursal){
+        $queryNombre = "SELECT * FROM PRODUCTO WHERE (SUCURSAL_PRODUCTO) like ('%$sucursal%')";
+        $link = Conexion::conecta();
+        $resultadoNombre = mysqli_query($link, $queryNombre);
+        mysqli_close($link);
+        return $resultadoNombre;
+        }
+        
+    }*/
+?>
